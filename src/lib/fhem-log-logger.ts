ddf1075b5b93f8c48008ec0f7c748aaceea3103e
derkallevombau <kalle@Aspire2>
2021-02-22 18:08:40 +0100

    fhem-log-logger.ts: Ported to TypeScript.

--- src/lib/fhem-log-logger.ts
+++ src/lib/fhem-log-logger.ts
@@ -1,25 +1,28 @@
 /**
- * @file tuyadevctlsrv.js
- * Created on: Nov 24 2019
- * @author derkallevombau
+ * fhem-log-logger.ts
+ * Author: derkallevombau
+ * Created: Nov 24 2019
  */
 
-const strftime = require('strftime');
+// For strftime
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+
+import strftime = require('strftime');
 
 /**
  * Logger for first stage of application startup.
  * Since `console` outputs to stdout/stderr and these
  * are redirected to FHEM log, it uses the same formatting.
  */
-const fhemLogLogger =
+export default class FhemLogLogger
 {
-	debug: (message, ...optionalParams) => console.debug(strftime('%Y.%m.%d %H:%M:%S'), '4:', 'tuyadevctlsrv-fhem:', message, ...optionalParams),
-	info:  (message, ...optionalParams) => console.info(strftime('%Y.%m.%d %H:%M:%S'), '3:', 'tuyadevctlsrv-fhem:', message, ...optionalParams),
-	warn:  (message, ...optionalParams) => console.warn(strftime('%Y.%m.%d %H:%M:%S'), '2:', 'tuyadevctlsrv-fhem:', message, ...optionalParams),
-	error: (message, ...optionalParams) => console.error(strftime('%Y.%m.%d %H:%M:%S'), '1:', 'tuyadevctlsrv-fhem:', message, ...optionalParams),
-	fatal: (message, ...optionalParams) => console.error(strftime('%Y.%m.%d %H:%M:%S'), '1:', 'tuyadevctlsrv-fhem:', message, ...optionalParams),
-};
+	static debug(message: any, ...optionalParams: any[]): void { console.debug(strftime('%Y.%m.%d %H:%M:%S'), '4:', 'tuyadevctlsrv-fhem:', message, ...optionalParams); }
+	static info(message: any, ...optionalParams: any[]): void  { console.info(strftime('%Y.%m.%d %H:%M:%S'), '3:', 'tuyadevctlsrv-fhem:', message, ...optionalParams); }
+	static warn(message: any, ...optionalParams: any[]): void  { console.warn(strftime('%Y.%m.%d %H:%M:%S'), '2:', 'tuyadevctlsrv-fhem:', message, ...optionalParams); }
+	static error(message: any, ...optionalParams: any[]): void { console.error(strftime('%Y.%m.%d %H:%M:%S'), '1:', 'tuyadevctlsrv-fhem:', message, ...optionalParams); }
+	static fatal(message: any, ...optionalParams: any[]): void { console.error(strftime('%Y.%m.%d %H:%M:%S'), '1:', 'tuyadevctlsrv-fhem:', message, ...optionalParams); }
 
-fhemLogLogger.log = (level, ...args) => fhemLogLogger[level](...args);
-
-module.exports = fhemLogLogger;
+	// N.B.: - Using "String literal types" available since TS 4.1.
+	//       - We can access static class mebers via subscript operator like with "legacy" objects, even from inside the class.
+	static log(level: 'debug' | 'info' | 'warn' | 'error' | 'fatal', message: any, ...optionalParams: any[]): void { FhemLogLogger[level](message, ...optionalParams); }
+}

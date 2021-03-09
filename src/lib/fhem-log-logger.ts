321ffe2b9637743948bb6040740e7b33ef641a4b
derkallevombau <kalle@Aspire2>
2021-03-09 17:01:58 +0100

    fhem-log-logger: Changed FHEM verbose levels, use 'debug' for debug method.

--- src/lib/fhem-log-logger.ts
+++ src/lib/fhem-log-logger.ts
@@ -16,14 +16,14 @@ import strftime = require('strftime');
  */
 const FhemLogLogger =
 {
-	debug(message: any, ...args: any[]): void { console.debug(strftime('%Y.%m.%d %H:%M:%S'), '4:', 'tuyadevctlsrv-fhem:', message, ...args); },
-	info(message: any, ...args: any[]): void { console.info(strftime('%Y.%m.%d %H:%M:%S'), '3:', 'tuyadevctlsrv-fhem:', message, ...args); },
-	warn(message: any, ...args: any[]): void { console.warn(strftime('%Y.%m.%d %H:%M:%S'), '2:', 'tuyadevctlsrv-fhem:', message, ...args); },
+	debug(message: any, ...args: any[]): void { console.debug(strftime('%Y.%m.%d %H:%M:%S'), 'debug:', 'tuyadevctlsrv-fhem:', message, ...args); },
+	info(message: any, ...args: any[]): void { console.info(strftime('%Y.%m.%d %H:%M:%S'), '2:', 'tuyadevctlsrv-fhem:', message, ...args); },
+	warn(message: any, ...args: any[]): void { console.warn(strftime('%Y.%m.%d %H:%M:%S'), '1:', 'tuyadevctlsrv-fhem:', message, ...args); },
 	error(message: any, ...args: any[]): void { console.error(strftime('%Y.%m.%d %H:%M:%S'), '1:', 'tuyadevctlsrv-fhem:', message, ...args); },
-	fatal(message: any, ...args: any[]): void { console.error(strftime('%Y.%m.%d %H:%M:%S'), '1:', 'tuyadevctlsrv-fhem:', message, ...args); },
+	fatal(message: any, ...args: any[]): void { console.error(strftime('%Y.%m.%d %H:%M:%S'), '0:', 'tuyadevctlsrv-fhem:', message, ...args); },
 
 	// N.B.: - Using "String literal types" available since TS 4.1.
-	//       - We can access the object's properties from inside the object via the variable we assigned it to.
+	//       - We can access the object's properties from inside the object.
 	log(level: 'debug' | 'info' | 'warn' | 'error' | 'fatal', message: any, ...args: any[]): void { FhemLogLogger[level](message, ...args); }
 };
 
